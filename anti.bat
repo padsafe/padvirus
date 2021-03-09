a75dacc97c11b85be881ba475b43e573daada801
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-09 16:33:24 -0600

    Update anti.bat

--- anti.bat
+++ anti.bat
@@ -2,12 +2,13 @@
 title PadVirus
 echo Scanning for viruses
 :start
-IF EXIST virus.bat goto infected
-IF EXIST Virus.bat goto infected
-IF EXIST virus.exe goto infected
-IF EXIST Virus.exe goto infected
+IF EXIST virus.bat goto infected1
+IF EXIST Virus.bat goto infected2
+IF EXIST virus.exe goto infected3
+IF EXIST Virus.exe goto infected4
 cd C:\Windows\system32
 
 
-:infected
+:infected1
 echo Infected file detected. We are cleaning it up right now!
+del virus.bat
