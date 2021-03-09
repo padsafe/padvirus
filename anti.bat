a29c5b8401c430b2e99abf100731f39dd98b39c7
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-09 16:42:05 -0600

    Update anti.bat

--- anti.bat
+++ anti.bat
@@ -8,6 +8,7 @@ IF EXIST virus.exe goto infected3
 IF EXIST Virus.exe goto infected4
 IF EXIST memz.exe goto infected5
 IF EXIST Memz.exe goto infected6
+IF EXIST betterdiscord.exe goto infected7
 cd C:\Windows\system32
 
 
@@ -34,3 +35,11 @@ del memz.exe
 :infected6
 echo Infected file detected. We are cleaning it up right now!
 del Memz.exe
+
+:infected7
+echo Infected file detected. We are cleaning it up right now!
+del betterdiscord.exe
+
+:infected8
+echo Infected file detected. We are cleaning it up right now!
+del BetterDiscord.exe
