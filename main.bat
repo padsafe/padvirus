5146aed73faaa8ee79248cb92245b68973d6ccf2
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 15:11:21 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -8,9 +8,11 @@ cd C:\Windows\system32
 
 :clean
 echo Scanning...
+ping -n 6 127.0.0.1>nul
 
 :infected
 echo [!] Infected file detected! (nitro.py)
 del nitro.py
 pause
+ping -n 6 127.0.0.1>nul
 goto start
