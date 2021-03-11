b72a45acf5f18aac54fbcc2674f5c275dadadef2
MelonSounds <69148613+MelonSounds@users.noreply.github.com>
2021-03-11 15:25:56 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -4,6 +4,8 @@ echo Scanning...
 :start
 IF EXIST nitro.py goto infected
 IF NOT EXIST nitro.py goto clean
+IF EXIST Nitro.py goto infected1
+IF NOT EXISR Nitro.py goto clean
 cd C:\Windows\system32
 
 :clean
@@ -12,6 +14,13 @@ ping -n 2 127.0.0.1>nul
 
 :infected
 echo [!] Infected file detected! (nitro.py)
+del nitro.py
+pause
+ping -n 2 127.0.0.1>nul
+goto start
+
+:infected1
+echo [!] Infected file detected! (Nitro.py)
 del nitro.py
 pause
 ping -n 2 127.0.0.1>nul
